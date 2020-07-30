d1d3bef173f8c0f7201dae97cf7d61477b14b53c
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2020-07-31 10:06:33 +1200

    Databse work so far

---
+++ topbit.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Book Review Database">
+    <meta name="keywords" content="reviews, books, non-fiction, fiction, reading, genre, reading, enjoy, dislike, favourite, pages, title">
+    <meta name="author" content="Aref Osman">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Book Review Database</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/no_side.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        
+        <div class="box banner">
+            
+        <!-- logo image linking to home page goes here -->
+        <a href="index.php">
+            <div class="logo"  title="Logo - Click here to go to the Home Page">
+            <img class="img-circle" src="Images/Book%20image.png" width="150" height="150" alt="Book logo" border-radius= "10px"/>
+            
+            </div>    <!-- / logo -->
+        </a>
+            
+            <h1>Epic Reading</h1>
+        </div>    <!-- / banner -->
+        
+        
+        <div class="box side">
+        
+        
+            <h2>Search | <a class="side" href="showall.php">Show All</a></h2>
+            
+            <i>Type part of the title/author name if desired</i>
+            
+            <hr />
+            
+            Title Search<br />
+            Author Search<br />
+            Genre Search<br />
+            Rating Search
+        
+        </div> <!-- / side bar -->
